64f30d02fb4d892cdb1f2cf8dda559c849085bea
Valvon <evolutiongearstv@gmail.com>
2024-11-22 20:13:31 +0100

    Set targetComponent explicitly

--- src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/effectiveConfig/EffectiveConfigToolBar.kt
+++ src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/effectiveConfig/EffectiveConfigToolBar.kt
@@ -4,14 +4,15 @@ import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.DefaultActionGroup
 import javax.swing.JComponent
 
-class EffectiveConfigToolBar(private val toolWindow: EffectiveConfigToolWindow)  {
-    fun createToolbar() : JComponent {
+class EffectiveConfigToolBar(private val toolWindow: EffectiveConfigToolWindow) {
+    fun createToolbar(): JComponent {
         val actionGroup = DefaultActionGroup().apply {
             add(RefreshAction(toolWindow))
         }
 
         val actionManager = ActionManager.getInstance()
         val toolBar = actionManager.createActionToolbar("EffectiveConfigToolBar", actionGroup, true)
+        toolBar.targetComponent = toolWindow
         return toolBar.component
     }
 }
