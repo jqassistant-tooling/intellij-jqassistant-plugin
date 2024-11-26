5985cae85683d493940a43ae7fcbffefd5114644
Valvon <valentin.ehrhardt@gmx.de>
2024-11-26 15:03:51 +0100

    #17 Remove unused imports

--- src/main/kotlin/org/jqassistant/tooling/intellij/plugin/data/config/JqaConfigFileProvider.kt
+++ src/main/kotlin/org/jqassistant/tooling/intellij/plugin/data/config/JqaConfigFileProvider.kt
@@ -6,7 +6,6 @@ import com.intellij.openapi.editor.event.DocumentEvent
 import com.intellij.openapi.editor.event.DocumentListener
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.newvfs.events.VFileEvent
 import com.intellij.psi.search.FileTypeIndex
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.util.application
