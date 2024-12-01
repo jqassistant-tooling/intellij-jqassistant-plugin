06f34821c648fb5344fb0ee9c642bd1ca46736fe
Valvon <evolutiongearstv@gmail.com>
2024-12-01 12:21:06 +0100

    #17 Apply much nicer AnimatedIcon

--- src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/config/toolwindow/LoadingPanel.kt
+++ src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/config/toolwindow/LoadingPanel.kt
@@ -1,14 +1,14 @@
 package org.jqassistant.tooling.intellij.plugin.editor.config.toolwindow
 
+import com.intellij.ui.AnimatedIcon
 import java.awt.BorderLayout
-import javax.swing.ImageIcon
 import javax.swing.JLabel
 import javax.swing.JPanel
 
 class LoadingPanel : JPanel() {
     init {
         layout = BorderLayout()
-        val loadingIcon = ImageIcon(javaClass.getResource("/icons/loading.gif"))
+        val loadingIcon = AnimatedIcon.Default()
         val loadingLabel = JLabel("Loading...", loadingIcon, JLabel.CENTER)
         add(loadingLabel, BorderLayout.CENTER)
     }
