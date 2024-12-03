ddf7671884269afd9825f178c7d101ebee00ba28
Valvon <evolutiongearstv@gmail.com>
2024-12-03 19:07:21 +0100

    Not working

--- src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/rules/refactoring/delete/RuleRefactoringSupportProvider.kt
+++ src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/rules/refactoring/delete/RuleRefactoringSupportProvider.kt
@@ -3,7 +3,10 @@ package org.jqassistant.tooling.intellij.plugin.editor.rules.refactoring.delete
 import com.intellij.lang.refactoring.RefactoringSupportProvider
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.util.elementType
+import com.intellij.psi.xml.XmlElementType
 import com.intellij.psi.xml.XmlTag
+import com.intellij.util.containers.addIfNotNull
 import com.intellij.util.xml.DomManager
 import org.jqassistant.tooling.intellij.plugin.data.rules.xml.RuleBase
 
@@ -16,6 +19,15 @@ class RuleRefactoringSupportProvider : RefactoringSupportProvider() {
         val xmlTag = PsiTreeUtil.getParentOfType(element, XmlTag::class.java)
         val manager = DomManager.getDomManager(element.project)
         val domElement = manager.getDomElement(xmlTag)
+
+        /* Clicking the <tag> element will return the parent <tag> element,
+         * which gives us no information about the rule type or if it is one
+         */
+        if (element.context.elementType == XmlElementType.XML_TAG) {
+            val elements: MutableList<XmlTag> = mutableListOf()
+            element.children.forEach { elements.addIfNotNull(PsiTreeUtil.getParentOfType(it, XmlTag::class.java)) }
+            elements.forEach { println(it) }
+        }
         println("available: $element, $xmlTag, $manager, $domElement, ${domElement is RuleBase}")
         return domElement is RuleBase
     }
