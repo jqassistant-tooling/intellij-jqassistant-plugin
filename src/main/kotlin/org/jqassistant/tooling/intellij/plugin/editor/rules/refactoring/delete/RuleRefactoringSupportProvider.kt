c1621ef0f0bf5e85e23e773be0894b8fb80aa071
Valvon <evolutiongearstv@gmail.com>
2024-12-03 17:05:48 +0100

    #13 Add working RuleRefactoringSupportProvider,  SafeDeleteProcessor is not called for some reason

--- src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/rules/refactoring/delete/RuleRefactoringSupportProvider.kt
+++ src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/rules/refactoring/delete/RuleRefactoringSupportProvider.kt
@@ -2,7 +2,21 @@ package org.jqassistant.tooling.intellij.plugin.editor.rules.refactoring.delete
 
 import com.intellij.lang.refactoring.RefactoringSupportProvider
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.xml.XmlTag
+import com.intellij.util.xml.DomManager
+import org.jqassistant.tooling.intellij.plugin.data.rules.xml.RuleBase
 
 class RuleRefactoringSupportProvider : RefactoringSupportProvider() {
-    override fun isSafeDeleteAvailable(element: PsiElement): Boolean = true
+    /**
+     * Checks if element is eligible for safe delete refactoring.
+     * Currently only works on the id attribute of a rule
+     */
+    override fun isSafeDeleteAvailable(element: PsiElement): Boolean {
+        val xmlTag = PsiTreeUtil.getParentOfType(element, XmlTag::class.java)
+        val manager = DomManager.getDomManager(element.project)
+        val domElement = manager.getDomElement(xmlTag)
+        println("available: $element, $xmlTag, $manager, $domElement, ${domElement is RuleBase}")
+        return domElement is RuleBase
+    }
 }
--- src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/rules/refactoring/delete/RuleSafeDeleteProcessorDelegate.kt
+++ src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/rules/refactoring/delete/RuleSafeDeleteProcessorDelegate.kt
@@ -1,87 +1,95 @@
 package org.jqassistant.tooling.intellij.plugin.editor.rules.refactoring.delete
 
-import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.xml.XmlTag
 import com.intellij.refactoring.safeDelete.NonCodeUsageSearchInfo
+import com.intellij.refactoring.safeDelete.SafeDeleteProcessor
 import com.intellij.refactoring.safeDelete.SafeDeleteProcessorDelegate
 import com.intellij.usageView.UsageInfo
+import com.intellij.util.containers.map2Array
+import com.intellij.util.xml.DomManager
+import org.jqassistant.tooling.intellij.plugin.data.rules.xml.RuleBase
 
 class RuleSafeDeleteProcessorDelegate : SafeDeleteProcessorDelegate {
-    private val logger = Logger.getInstance(RuleSafeDeleteProcessorDelegate::class.java)
-
-    init {
-        println("RuleSafeDeleteProcessorDelegate")
-        logger.info("RuleSafeDeleteProcessorDelegate instantiated")
-    }
-
     override fun findUsages(
         p0: PsiElement,
         p1: Array<out PsiElement>,
         p2: MutableList<in UsageInfo>,
     ): NonCodeUsageSearchInfo? {
-        TODO("Not yet implemented")
-        // search the whole project for usages of the rule
+        val xmlTag = PsiTreeUtil.getParentOfType(p0, com.intellij.psi.xml.XmlTag::class.java)
+        val manager = DomManager.getDomManager(p0.project)
+        val domElement = manager.getDomElement(xmlTag) as? RuleBase ?: return null
+        println("called findUsages")
+        // Search for all usages of the element with the same ID
+        val project = p0.project
+        val id = domElement.id.value
+        SafeDeleteProcessor.findGenericElementUsages(
+            p0,
+            p2,
+            p1,
+            GlobalSearchScope.projectScope(p0.getProject()),
+        )
+
+        return NonCodeUsageSearchInfo(SafeDeleteProcessor.getDefaultInsideDeletedCondition(p1), p0)
     }
 
     override fun getElementsToSearch(
         p0: PsiElement,
         p1: MutableCollection<out PsiElement>,
-    ): MutableCollection<out PsiElement>? {
-        TODO("Not yet implemented")
-    }
+    ): MutableCollection<out PsiElement>? = p1
 
     override fun getAdditionalElementsToDelete(
         p0: PsiElement,
         p1: MutableCollection<out PsiElement>,
         p2: Boolean,
-    ): MutableCollection<PsiElement>? {
-        TODO("Not yet implemented")
-    }
+    ): MutableCollection<PsiElement>? = null
 
     override fun findConflicts(
         p0: PsiElement,
         p1: Array<out PsiElement>,
-    ): MutableCollection<String>? {
-        TODO("Not yet implemented")
-    }
+    ): MutableCollection<String>? = null
 
     override fun preprocessUsages(
         p0: Project,
         p1: Array<out UsageInfo>,
-    ): Array<UsageInfo>? {
-        TODO("Not yet implemented")
-    }
+    ): Array<UsageInfo> = p1.map2Array { it }
 
     override fun prepareForDeletion(p0: PsiElement) {
-        TODO("Not yet implemented")
     }
 
-    override fun isToSearchInComments(p0: PsiElement?): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun isToSearchInComments(p0: PsiElement?): Boolean = false
 
     override fun setToSearchInComments(
         p0: PsiElement?,
         p1: Boolean,
     ) {
-        TODO("Not yet implemented")
+        // do nothing
     }
 
-    override fun isToSearchForTextOccurrences(p0: PsiElement?): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun isToSearchForTextOccurrences(p0: PsiElement?): Boolean = false
 
     override fun setToSearchForTextOccurrences(
         p0: PsiElement?,
         p1: Boolean,
     ) {
-        TODO("Not yet implemented")
+        // do nothing
     }
 
     override fun handlesElement(p0: PsiElement?): Boolean {
-        println("handlesElement")
-        return true
-        // TODO("Not yet implemented")
+        // DOM - Modell
+        // get the xml tag from the psi element
+        // dom element
+        // name
+        // indexing service with name
+        // is there a rule with this name?
+        println("handlesElement: $p0")
+        val xmlTag = PsiTreeUtil.getParentOfType(p0, XmlTag::class.java)
+        val manager = DomManager.getDomManager(p0?.project)
+        val domElement = manager.getDomElement(xmlTag)
+        println("handle: $p0, $xmlTag, $manager, $domElement, ${domElement is RuleBase}")
+        return domElement is RuleBase
     }
 }
