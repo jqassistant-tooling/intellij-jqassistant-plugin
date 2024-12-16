9456cc8d87a1153e1490e7f5c15616cafbf76cd0
Valvon <valentin.ehrhardt@gmx.de>
2024-12-16 10:07:06 +0100

    #10 Annotate rules that don't have any reference path to the base rule (fixed base rule atm)

--- src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/rules/annotator/FindUnusedAnnotator.kt
+++ src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/rules/annotator/FindUnusedAnnotator.kt
@@ -6,6 +6,7 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiReference
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.search.searches.ReferencesSearch
+import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.xml.XmlTag
 import com.intellij.usageView.UsageInfo
 import com.intellij.util.Query
@@ -24,16 +25,10 @@ class FindUnusedAnnotator : Annotator {
 
         // Check if element has a reference path from main parent group
         val id : PsiElement = tag?.getAttribute("id")?.valueElement?.navigationElement ?: element
-        val searchScope = GlobalSearchScope.projectScope(element.project)
-        val query: Query<PsiReference> = ReferencesSearch.search(id, searchScope)
-        val references = mutableListOf<PsiElement>()
-        query.forEach { reference ->
-            val usageInfo = UsageInfo(reference)
-            references.addIfNotNull(usageInfo.element)
-            }
+        val result = searchBaseReferenceRecursive(id, null)
 
         // If not referenced, add warning
-        if(references.isEmpty()){
+        if(!result){
             holder
                 .newAnnotation(
                     com.intellij.lang.annotation.HighlightSeverity.WARNING,
@@ -45,6 +40,40 @@ class FindUnusedAnnotator : Annotator {
         // TODO check if the rule is used in the rule set (can be referenced, but from places that are also incative)
 
     }
+// TODO make baseElement not nullable (nullable only for development)
+    private fun searchBaseReferenceRecursive(element: PsiElement, baseElement: PsiElement?) : Boolean{
+        val searchScope = GlobalSearchScope.projectScope(element.project)
+        val query: Query<PsiReference> = ReferencesSearch.search(element, searchScope)
+        val references = mutableListOf<PsiElement>()
+        query.forEach { reference ->
+            val usageInfo = UsageInfo(reference)
+            references.addIfNotNull(usageInfo.element)
+        }
 
+        var result = false
+        if (references.isEmpty()){
+            result =  false
+        } else if (references.contains(baseElement)){
+            result =  true
+        } else {
+            references.forEach { reference ->
+                val parent = PsiTreeUtil.getParentOfType(reference, XmlTag::class.java)
+                val parent2 = PsiTreeUtil.getParentOfType(parent, XmlTag::class.java)
+                val text = parent2?.getAttributeValue("id")
+                val nextSearch = parent2?.getAttribute("id")?.valueElement?.navigationElement
+                if(text == "biojava:Default") {
+                    result = true
+                    return true
+                } else if (nextSearch != null){
+                    result = searchBaseReferenceRecursive(nextSearch, baseElement)
+                }
+            }
+        }
+        return result
+    }
+
+    private fun findBaseJqaGroup() : PsiElement?{
+    return null
+    }
 
 }
