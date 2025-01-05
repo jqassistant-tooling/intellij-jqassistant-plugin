b25b4902b2c9650e315c2de2b9c7b4a4bcf635ae
Valvon <evolutiongearstv@gmail.com>
2025-01-05 20:16:18 +0100

    #70 Fix Override-only API usage error

--- src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/config/toolwindow/OutdatedConfigBannerPanel.kt
+++ src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/config/toolwindow/OutdatedConfigBannerPanel.kt
@@ -1,50 +1,19 @@
 package org.jqassistant.tooling.intellij.plugin.editor.config.toolwindow
 
 import com.intellij.openapi.actionSystem.ActionManager
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.DataContext
-import com.intellij.openapi.actionSystem.PlatformDataKeys
-import com.intellij.openapi.actionSystem.Presentation
-import com.intellij.openapi.project.Project
 import com.intellij.ui.EditorNotificationPanel
 
 /**
  *  Banner that is shown, when the configuration is outdated
  *  */
 class OutdatedConfigBannerPanel(
-    private val project: Project,
     private val action: RefreshAction,
 ) : EditorNotificationPanel(Status.Warning) {
     init {
         text = "Configuration files have changed. Configuration might be outdated."
         createActionLabel("Refresh") {
-            callRefreshAction()
+            val actionManager = ActionManager.getInstance()
+            actionManager.tryToExecute(action, null, null, null, true)
         }
-    }
-
-    /**
-     * Refreshes the toolwindow content
-     * */
-    private fun callRefreshAction() {
-        val dataContext =
-            DataContext { key ->
-                when (key) {
-                    PlatformDataKeys.PROJECT.name -> project
-                    else -> null
-                }
-            }
-
-        val presentation = Presentation().apply { copyFrom(action.templatePresentation) }
-        val event =
-            AnActionEvent(
-                null,
-                dataContext,
-                "EditorNotificationPanel",
-                presentation,
-                ActionManager.getInstance(),
-                0,
-            )
-        action.actionPerformed(event)
-        this.isVisible = false
     }
 }
