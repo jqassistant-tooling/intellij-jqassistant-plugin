a6014f7938c6fad1e0125c59fb727f720ffd878d
AKORA <65976562+A-K-O-R-A@users.noreply.github.com>
2025-01-07 21:24:26 +0100

    update toolwindow variables

--- src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/graph/OpenGraphAction.kt
+++ src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/graph/OpenGraphAction.kt
@@ -3,48 +3,52 @@ package org.jqassistant.tooling.intellij.plugin.editor.graph
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.components.service
+import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.pom.PomTargetPsiElement
-import org.jqassistant.tooling.intellij.plugin.data.rules.JqaRuleIndexingService
+import com.intellij.util.xml.DomTarget
 import org.jqassistant.tooling.intellij.plugin.data.rules.xml.Concept
 import org.jqassistant.tooling.intellij.plugin.data.rules.xml.Constraint
 import org.jqassistant.tooling.intellij.plugin.data.rules.xml.Group
 import org.jqassistant.tooling.intellij.plugin.data.rules.xml.RuleBase
 
 class OpenGraphAction : AnAction() {
+    companion object {
+        const val TOOL_WINDOW_ID: String = "Rule Graph"
+    }
+
     override fun actionPerformed(event: AnActionEvent) {
         val currentElement = event.getData(CommonDataKeys.PSI_ELEMENT) ?: return
-
         val domElement = currentElement as? PomTargetPsiElement ?: return
 
-        domElement.reference
-
-        val target = domElement.target as? RuleBase ?: return
+        val target = domElement.target as? DomTarget ?: return
+        val ruleBase = target.domElement as? RuleBase ?: return
 
         val project = event.project ?: return
-        val service = project.service<JqaRuleIndexingService>()
+        val toolWindow = ToolWindowManager.getInstance(project).getToolWindow(TOOL_WINDOW_ID) ?: return
 
-        when (target) {
+        val content = toolWindow.contentManager.getContent(0) ?: return
+        val component = content.component as? GraphToolWindowContent ?: return
+
+        component.currentRule = ruleBase
+        when (ruleBase) {
             is Concept -> {
-                val provides = target.providesConcept
-                val requires = target.requiresConcept
+                component.concepts = ruleBase.providesConcept.toMutableList()
+                component.concepts.addAll(0, ruleBase.requiresConcept)
+                component.constraints = mutableListOf()
+                component.groups = mutableListOf()
             }
 
             is Constraint -> {
-                val requires = target.requiresConcept
+                component.concepts = ruleBase.requiresConcept.toMutableList()
+                component.constraints = mutableListOf()
+                component.groups = mutableListOf()
             }
 
             is Group -> {
-                val groups = target.includeGroup
-                val constraints = target.includeConstraint
-                val concepts = target.includeConcept
-
-                println(groups)
-                println(constraints)
-                println(concepts)
+                component.groups = ruleBase.includeGroup.toMutableList()
+                component.constraints = ruleBase.includeConstraint.toMutableList()
+                component.concepts = ruleBase.includeConcept.toMutableList()
             }
         }
-
-        println(domElement)
     }
 }
