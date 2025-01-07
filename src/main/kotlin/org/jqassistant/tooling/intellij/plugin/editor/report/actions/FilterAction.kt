5ede04ca1ea03b8b57b504630f4f3d81c19fd8fe
AKORA <65976562+A-K-O-R-A@users.noreply.github.com>
2025-01-07 12:30:02 +0100

    Re-exapand elements after filtering

--- src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/report/actions/FilterAction.kt
+++ src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/report/actions/FilterAction.kt
@@ -3,7 +3,9 @@ package org.jqassistant.tooling.intellij.plugin.editor.report.actions
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.ui.getTreePath
 import com.intellij.ui.treeStructure.Tree
+import org.jqassistant.tooling.intellij.plugin.editor.report.tree.ReportNode
 import org.jqassistant.tooling.intellij.plugin.editor.report.tree.ReportTreeModel
 
 class FilterAction(
@@ -15,9 +17,18 @@ class FilterAction(
     ) {
     override fun actionPerformed(e: AnActionEvent) {
         for (tree in trees) {
+            // Save all expanded paths
+            val root = tree.model.root as ReportNode
+            val expandedPaths = tree.getExpandedDescendants(tree.model.getTreePath(root.userObject))
+
             val model = tree.model as? ReportTreeModel ?: continue
             // Notify tree so repaint happens
             model.reload()
+
+            // Re-expand all previously expanded paths
+            for (path in expandedPaths) {
+                tree.expandPath(path)
+            }
         }
     }
 }
