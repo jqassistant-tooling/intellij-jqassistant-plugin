bc7279534f0dd4848959a92c9de88ad18730de91
AKORA <65976562+A-K-O-R-A@users.noreply.github.com>
2025-01-17 17:06:44 +0100

    Use new configuration service

--- src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/graph/OpenGraphAction.kt
+++ src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/graph/OpenGraphAction.kt
@@ -1,11 +1,15 @@
 package org.jqassistant.tooling.intellij.plugin.editor.graph
 
+import com.buschmais.jqassistant.core.rule.api.model.Rule
+import com.buschmais.jqassistant.core.rule.api.model.RuleSet
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.components.service
 import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.pom.PomTargetPsiElement
 import com.intellij.util.xml.DomTarget
+import org.jqassistant.tooling.intellij.plugin.data.config.JqaConfigurationService
 import org.jqassistant.tooling.intellij.plugin.data.rules.xml.RuleBase
 
 class OpenGraphAction : AnAction() {
@@ -26,8 +30,20 @@ class OpenGraphAction : AnAction() {
         val content = toolWindow.contentManager.getContent(0) ?: return
         val component = content.component as? GraphToolWindowContent ?: return
 
-        component.currentRule = ruleBase
+        val configurationService = project.service<JqaConfigurationService>()
+        val ruleSet = configurationService.getAvailableRules()
 
-        component.buildGraph()
+        val currentRule = getAllRules(ruleSet).find { r -> r.id == ruleBase.id.stringValue } ?: return
+
+        component.refreshGraph(currentRule, ruleSet)
+    }
+
+    private fun getAllRules(ruleSet: RuleSet): List<Rule> {
+        val rules = mutableListOf<Rule>()
+        rules += ruleSet.groupsBucket.all
+        rules += ruleSet.conceptBucket.all
+        rules += ruleSet.constraintBucket.all
+
+        return rules
     }
 }
