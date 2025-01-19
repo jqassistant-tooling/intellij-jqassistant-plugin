c512e57dc04b37ec48185840b88bb5c80dba0f2b
HolonProduction <holonproduction@gmail.com>
2025-01-19 22:29:17 +0100

    Data+Editor: Filter completion by type

--- src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/rules/XmlRuleReferenceConverter.kt
+++ src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/rules/XmlRuleReferenceConverter.kt
@@ -5,14 +5,23 @@ import com.intellij.psi.PsiReference
 import com.intellij.util.xml.ConvertContext
 import com.intellij.util.xml.CustomReferenceConverter
 import com.intellij.util.xml.GenericDomValue
+import org.jqassistant.tooling.intellij.plugin.data.rules.JqaRuleType
 
-class XmlRuleReferenceConverter : CustomReferenceConverter<String> {
+open class XmlRuleReferenceConverter(
+    private val type: JqaRuleType? = null,
+) : CustomReferenceConverter<String> {
     override fun createReferences(
         value: GenericDomValue<String>?,
         element: PsiElement?,
         context: ConvertContext?,
     ): Array<PsiReference> {
         if (element == null) return emptyArray()
-        return arrayOf(RuleReference(element, value?.value ?: element.text))
+        return arrayOf(RuleReference(element, value?.value ?: element.text, type))
     }
 }
+
+class XmlGroupReferenceConverter : XmlRuleReferenceConverter(JqaRuleType.GROUP)
+
+class XmlConceptReferenceConverter : XmlRuleReferenceConverter(JqaRuleType.CONCEPT)
+
+class XmlConstraintReferenceConverter : XmlRuleReferenceConverter(JqaRuleType.CONSTRAINT)
