a386f92815009d06f0c5c7760c9a4f156cba75d1
valle <104097969+Valvon@users.noreply.github.com>
2025-01-22 11:05:01 +0100

    Add settings UI (#105)
    * Add UI Components
    
    * Add file and directory validity checks
    
    * Update FileChooser and add relative paths
    
    * Make null be the default
    
    * Add tooltips and fix broken textfield
    
    * Minor fixes
    
    * Minor fixes
    
    * Add UI Components
    
    * Add file and directory validity checks
    
    * Update FileChooser and add relative paths
    
    * Make null be the default
    
    * Add tooltips and fix broken textfield
    
    * Minor fixes
    
    * Minor fixes
    
    * Add Warning label if Maven is not supported
    
    * Fix state not properly stored
    
    * Add requested changes
    
    * Update src/main/kotlin/org/jqassistant/tooling/intellij/plugin/settings/PluginSettings.kt
    
    Co-authored-by: HolonProduction <holonproduction@gmail.com>
    
    * Update src/main/kotlin/org/jqassistant/tooling/intellij/plugin/settings/PluginSettings.kt
    
    Co-authored-by: HolonProduction <holonproduction@gmail.com>
    
    * Add requested changes
    
    * Remove defaultEmptyText
    
    * Fix capitalization and "reset" always being shown
    
    ---------
    
    Co-authored-by: HolonProduction <holonproduction@gmail.com>

---
+++ src/main/kotlin/org/jqassistant/tooling/intellij/plugin/settings/PluginSettings.kt
@@ -0,0 +1,51 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jqassistant.tooling.intellij.plugin.settings
+
+import com.intellij.openapi.components.BaseState
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.RoamingType
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import org.jqassistant.tooling.intellij.plugin.data.config.JqaDistribution
+
+/**
+ * Supports storing the application settings in a persistent way.
+ * The {@link com.intellij.openapi.components.State State} and {@link Storage}
+ * annotations define the name of the data and the filename where these persistent
+ * application settings are stored.
+ */
+
+@Service(Service.Level.PROJECT)
+@State(
+    name = "jqassistant.JqaPluginSettings",
+    storages = [Storage("jqaPluginSettings.xml", roamingType = RoamingType.PER_OS)],
+)
+internal class PluginSettings : PersistentStateComponent<PluginSettings.State> {
+    internal class State : BaseState() {
+        var distribution by enum(JqaDistribution.CLI)
+        var cliExecRootDir by string()
+        var cliParams by string()
+        var mavenProjectFile by string()
+        var mavenAdditionalProps by string()
+        var mavenProjectDescription by string()
+        var mavenScriptSourceDir by string()
+        var mavenOutputEncoding by string()
+    }
+
+    private var myState = State()
+
+    override fun getState(): State = myState
+
+    override fun loadState(state: State) {
+        myState = state
+    }
+
+    companion object {
+        fun getInstance(project: Project): PluginSettings = project.service<PluginSettings>()
+
+        const val DISPLAY_NAME = "jQAssistant Tooling"
+    }
+}
