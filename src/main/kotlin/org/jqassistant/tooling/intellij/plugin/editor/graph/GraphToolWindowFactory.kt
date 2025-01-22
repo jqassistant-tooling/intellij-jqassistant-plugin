d0711e21f535f5cb2509c67da8ca1ac123161e53
AKORA <65976562+A-K-O-R-A@users.noreply.github.com>
2025-01-22 20:38:27 +0100

    cleanup

--- src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/graph/GraphToolWindowFactory.kt
+++ src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/graph/GraphToolWindowFactory.kt
@@ -10,21 +10,15 @@ internal class GraphToolWindowFactory :
     ToolWindowFactory,
     DumbAware {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        loadToolWindowContent(project, toolWindow, false)
+        loadToolWindowContent(project, toolWindow)
     }
 
-    private fun loadToolWindowContent(project: Project, toolWindow: ToolWindow, forceReload: Boolean = false) {
+    private fun loadToolWindowContent(project: Project, toolWindow: ToolWindow) {
         val toolWindowContent = GraphToolWindowContent(project, toolWindow)
 
         // Add individual tabs for every base directory in the current project that contains a report xml file
         val content =
             ContentFactory.getInstance().createContent(toolWindowContent, "Graph", false)
         toolWindow.contentManager.addContent(content)
-    }
-
-    fun reloadToolWindow(project: Project, toolWindow: ToolWindow) {
-        toolWindow.contentManager.removeAllContents(true)
-
-        loadToolWindowContent(project, toolWindow, true)
     }
 }
