e670e7003667149323500e5951c4e6e90bc100f1
AKORA <65976562+A-K-O-R-A@users.noreply.github.com>
2025-01-24 11:42:01 +0100

    Use DomInspection instead

--- src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/rules/XmlRuleDomInspection.kt
+++ src/main/kotlin/org/jqassistant/tooling/intellij/plugin/editor/rules/XmlRuleDomInspection.kt
@@ -1,10 +1,14 @@
 package org.jqassistant.tooling.intellij.plugin.editor.rules
 
+import com.intellij.codeInsight.daemon.impl.analysis.RemoveTagIntentionFix
 import com.intellij.codeInsight.daemon.impl.analysis.XmlChangeAttributeValueIntentionFix
+import com.intellij.openapi.components.service
+import com.intellij.psi.xml.XmlTag
 import com.intellij.util.xml.DomElement
 import com.intellij.util.xml.highlighting.BasicDomElementsInspection
 import com.intellij.util.xml.highlighting.DomElementAnnotationHolder
 import com.intellij.util.xml.highlighting.DomHighlightingHelper
+import org.jqassistant.tooling.intellij.plugin.data.config.JqaConfigurationService
 import org.jqassistant.tooling.intellij.plugin.data.rules.xml.JqassistantRules
 import org.jqassistant.tooling.intellij.plugin.data.rules.xml.RuleBase
 import org.jqassistant.tooling.intellij.plugin.editor.MessageBundle
@@ -20,11 +24,31 @@ class XmlRuleDomInspection : BasicDomElementsInspection<JqassistantRules>(Jqassi
             is RuleBase -> {
                 val id = element.id.stringValue ?: return
 
+                // Check for reserved wildcard characters
                 if ("*" in id || "?" in id) {
                     holder.createProblem(
                         element.id,
                         MessageBundle.message("id.contains.wildcard.characters"),
                         XmlChangeAttributeValueIntentionFix(id.replace("*", "").replace("?", "")),
+                    )
+                }
+
+                // Check if this rule is used
+                val configService = element.manager.project.service<JqaConfigurationService>()
+
+                // Do not annotate if the effective rule set is currently unknown
+                val ruleSet = configService.getEffectiveRules() ?: return
+
+                val allRules = ruleSet.groups + ruleSet.concepts.keys + ruleSet.constraints.keys
+                val ruleUsed = allRules.find { rule -> rule.id == id } != null
+
+                val xmlElement = element.xmlElement as XmlTag
+
+                if (!ruleUsed) {
+                    holder.createProblem(
+                        element.id,
+                        MessageBundle.message("annotator.inactive.rule"),
+                        RemoveTagIntentionFix(xmlElement.name, xmlElement),
                     )
                 }
             }
